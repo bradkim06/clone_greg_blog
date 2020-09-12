8e03a611595f5fb4545e56100ed23bf5bf7ef224
bradkim06 <bradkim06@gmail.com>
2020-09-12 16:03:11 +0900

    color change

--- src/styles/darkColors.js
+++ src/styles/darkColors.js
@@ -2,11 +2,11 @@ module.exports = {
   background: "#222222",
   white: "#f5f5f5",
   accent: "#5ac18e",
-  bright: "#cccccc",
+  bright: "#b6b6b6",
   dark: "#333333",
   gray: "#555555",
-  lightGray: "#bbbbbb",
-  superLightGray: "#dedede",
+  lightGray: "#aaaaaa",
+  superLightGray: "#c2c2c2",
 
   palette: {
     second: "#baffc9",
--- src/styles/lightColors.js
+++ src/styles/lightColors.js
@@ -1,9 +1,9 @@
 module.exports = {
-  background: "#f5f5f5",
-  white: "#fcfcfc",
+  background: "#f0f0f0",
+  white: "#f5f5f5",
   bright: "#ededed",
-  dark: "#404040",
-  deepDark: "#323232",
+  dark: "#656565",
+  deepDark: "#5a5a5a",
   gray: "#757575",
   lightGray: "#b5b5b5",
   superLightGray: "#dbdbdb",
